dc3caff76ab9c3c1c8e4787bcc5841df6ccb8782
Kirk Byers <ktbyers@twb-tech.com>
2020-05-10 12:39:48 -0700

    Temporary code so tests will run

--- nornir_netmiko/tmp_glue/__init__.py
+++ nornir_netmiko/tmp_glue/__init__.py
@@ -1,7 +1,15 @@
 from nornir_netmiko.tmp_glue.print_result import print_result
-
+from nornir_netmiko.tmp_glue.load_json import load_json
+from nornir_netmiko.tmp_glue.load_yaml import load_yaml
+from nornir_netmiko.tmp_glue.template_file import template_file
+from nornir_netmiko.tmp_glue.template_string import template_string
+from nornir_netmiko.tmp_glue.write_file import write_file
 
 __all__ = (
     "print_result",
+    "load_json",
+    "load_yaml",
+    "template_file",
+    "template_string",
+    "write_file",
 )
-
